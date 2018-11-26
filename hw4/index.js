738898522aa6bdb4e1d298db89187ff70e8a0e5d
fak2116@columbia.edu <fak2116>
2018-11-26 01:57:15 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -46,11 +46,12 @@ function doPredict(predict) {
   const textField = document.getElementById('text-entry');
   const result = predict(textField.value);
   score_string = "Class scores: ";
-  console.log("result.score");
   for (var x in result.score) {
     score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
   }
-  console.log(Math.max(result.score[0], result.score[1], result.score[2]));
+  var maxlabel = Object.keys(result).reduce((a, b) => obj[a] > obj[b] ? a : b);
+  console.log("MaxLabel:")
+  console.log(maxlabel);
   //console.log(score_string);
   status(
       score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
